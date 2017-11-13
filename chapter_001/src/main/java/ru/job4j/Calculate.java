46928b607095e4bc4b357156732f1fbf51c59de5
Tolkachev Nikita <nictol76@yandex.ru>
2017-11-14 02:00:24 +0300

    1.3. JavaDoc. Code convention [#4087](2)

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+* Class Класс для выичсления арифметических операций + - * / ^.
+* @author ntolkachev
+* @since 14.11.17
+* @version 0.1
+*/
 public class Calculate {
+
+  /**
+  * Конструктор, вывод строки в консоль.
+  * @param arg - arg.
+  */
   public static void main (String[] Args) {
     System.out.println ("Hello world!");
   }
