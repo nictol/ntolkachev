96bf11a970a1f051640f09ef24173f0ff318f8db
Tolkachev Nikita <nictol76@gmail.com>
2017-11-16 02:54:16 +0300

    1.5. Checkstyle [#30857] (2)

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,28 +1,32 @@
 package ru.job4j;
 
 /**
-* Class Класс для выичсления арифметических операций + - * / ^.
-* @author ntolkachev (nictol76@gmail.com)
-* @since 14.11.17
-* @version 0.1
-*/
+ * Class Класс для выичсления арифметических операций + - * / ^.
+ *
+ * @author ntolkachev (nictol76@gmail.com)
+ * @version 0.1
+ * @since 14.11.17
+ */
 
 public class Calculate {
 
-  /**
-  * Method echo.
-  * @param name Your name.
-  * @return Echo plus your name.
-  */
-  public String echo(String name) {
-	return "Echo, echo, echo : " + name;
-  }
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 
-  /**
-  * Конструктор, вывод строки в консоль.
-  * @param arg - arg.
-  */
-  public static void main (String[] Args) {
-    System.out.println ("Hello world!");
-  }
+    /**
+     * Конструктор, вывод строки в консоль.
+     *
+     * @param args - arg.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
 }
