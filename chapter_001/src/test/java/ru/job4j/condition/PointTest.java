33594aba2baddc5d60d60aea05760d9c6e006e5b
Tolkachev Nikita <nictol76@gmail.com>
2017-11-18 00:08:51 +0300

    3.2. Положение точки [#188]

---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PointTest {
+    @Test
+    public void whenPointOnLineThenTrue() {
+        //create of new point.
+        Point a = new Point(1, 1);
+        // execute method - is and get result;
+        boolean rsl = a.is(0, 1);
+        // assert result by excepted value.
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void whenPointOffLineThenFalse() {
+        //create of new point.
+        Point a = new Point(5, 1);
+        // execute method - is and get result;
+        boolean rsl = a.is(8, 1);
+        // assert result by excepted value.
+        assertThat(rsl, is(false));
+    }
+}
