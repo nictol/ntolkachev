3d300da5823253d5aa5ea18961eb8c36acbdd999
Tolkachev Nikita <nictol76@gmail.com>
2017-12-10 17:26:58 +0300

    Проверка, что одно слово находится в другом слове [#228] (2)

--- chapter_001/src/main/java/ru/job4j/string/SubString.java
+++ chapter_001/src/main/java/ru/job4j/string/SubString.java
@@ -2,16 +2,20 @@ package ru.job4j.string;
 
 public class SubString {
     boolean contains(String origin, String sub) {
-        boolean result = false;
+
         for (int i = 0; i <= origin.length() - sub.length(); i++) {
             int k = i;
-            boolean containsSubstring = true;
+            boolean result = true;
+
             for (int j = 0; j < sub.length(); j++) {
-                if (sub.charAt(j) != origin.charAt(k)) containsSubstring = false;
+                if (sub.charAt(j) != origin.charAt(k)) result = false;
                 k++;
             }
-            if (containsSubstring) result = containsSubstring;
+
+            if (result) {
+                return true;
+            }
         }
-        return result;
+        return false;
     }
 }
