062b69d3c3b7fc31c02477cf507b56e84640c489
Tolkachev Nikita <nictol76@gmail.com>
2017-12-19 01:18:13 +0300

    Проверка, что одно слово находится в другом слове [#228] (3)

--- chapter_001/src/main/java/ru/job4j/string/SubString.java
+++ chapter_001/src/main/java/ru/job4j/string/SubString.java
@@ -3,9 +3,10 @@ package ru.job4j.string;
 public class SubString {
     boolean contains(String origin, String sub) {
 
+        boolean result = false;
         for (int i = 0; i <= origin.length() - sub.length(); i++) {
             int k = i;
-            boolean result = true;
+            result = true;
 
             for (int j = 0; j < sub.length(); j++) {
                 if (sub.charAt(j) != origin.charAt(k)) result = false;
@@ -13,9 +14,9 @@ public class SubString {
             }
 
             if (result) {
-                return true;
+                return result;
             }
         }
-        return false;
+        return result;
     }
 }
