f820881384411fe8cd757a70c3fe526f489ca557
Tolkachev Nikita <nictol76@gmail.com>
2018-07-12 18:13:43 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ chapter_003/src/main/java/search/PhoneBook.java
@@ -0,0 +1,32 @@
+package search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneBook {
+
+    private List<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+
+        for (Person user : persons) {
+            if ((user.getName().contains(key) || (user.getAddress().contains(key)) || (user.getPhone().contains(key)) || (user.getSurname().contains(key)))) {
+                result.add(user);
+            }
+
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/search/PhoneBookTest.java
@@ -0,0 +1,58 @@
+package search;
+
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneBookTest {
+    @Test
+    public void whenFindByName() {
+        PhoneBook phones = new PhoneBook();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Petr");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByPartOfName() {
+        PhoneBook phones = new PhoneBook();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Pe");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneBook phones = new PhoneBook();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Bry");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneBook phones = new PhoneBook();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("72");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindBySurname() {
+        PhoneBook phones = new PhoneBook();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("sent");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+}
