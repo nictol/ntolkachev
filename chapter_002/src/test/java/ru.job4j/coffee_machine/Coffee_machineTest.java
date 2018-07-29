d3b66037ac0f506d7cc76e48950b97993d3c9fbe
Tolkachev Nikita <nictol76@gmail.com>
2018-07-29 12:16:23 +0300

    Кофемашина. [#34741] (2)

--- chapter_002/src/test/java/ru.job4j/coffee_machine/Coffee_machineTest.java
+++ chapter_002/src/test/java/ru.job4j/coffee_machine/Coffee_machineTest.java
@@ -2,6 +2,9 @@ package ru.job4j.coffee_machine;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,10 +14,10 @@ public class Coffee_machineTest {
     public void testOne() {
 
         Сoffee_machine machne01 = new Сoffee_machine();
-        int[] result = machne01.change(50, 35);
-        int[] expexted = new int[10000];
-        expexted[0] = 10;
-        expexted[1] = 5;
+        List result = machne01.change(50, 35);
+        List expexted = new ArrayList<Integer>();
+        expexted.add(10);
+        expexted.add(5);
 
         assertThat(result, is(expexted));
     }
@@ -23,9 +26,9 @@ public class Coffee_machineTest {
     public void testTwo() {
 
         Сoffee_machine machne01 = new Сoffee_machine();
-        int[] result = machne01.change(100, 99);
-        int[] expexted = new int[10000];
-        expexted[0] = 1;
+        List result = machne01.change(100, 99);
+        List expexted = new ArrayList<Integer>();
+        expexted.add(1);
 
 
         assertThat(result, is(expexted));
@@ -35,8 +38,8 @@ public class Coffee_machineTest {
     public void testThree() {
 
         Сoffee_machine machne01 = new Сoffee_machine();
-        int[] result = machne01.change(100, 100);
-        int[] expexted = new int[10000];
+        List result = machne01.change(100, 100);
+        List expexted = new ArrayList<Integer>();
 
 
         assertThat(result, is(expexted));
@@ -46,11 +49,11 @@ public class Coffee_machineTest {
     public void testFour() {
 
         Сoffee_machine machne01 = new Сoffee_machine();
-        int[] result = machne01.change(50, 10);
-        int[] expexted = new int[10000];
+        List result = machne01.change(50, 10);
+        List expexted = new ArrayList<Integer>();
 
         for (int i = 0; i < 4; i++) {
-            expexted[i] = 10;
+            expexted.add(10);
         }
 
 
@@ -61,11 +64,11 @@ public class Coffee_machineTest {
     public void testFive() {
 
         Сoffee_machine machne01 = new Сoffee_machine();
-        int[] result = machne01.change(50, 0);
-        int[] expexted = new int[10000];
+        List result = machne01.change(50, 0);
+        List expexted = new ArrayList<Integer>();
 
         for (int i = 0; i < 5; i++) {
-            expexted[i] = 10;
+            expexted.add(10);
         }
 
         assertThat(result, is(expexted));
