9e0ccd0ff27fc7d230c260e8134bb56f22cd0c61
Tolkachev Nikita <nictol76@yandex.ru>
2019-12-26 17:11:49 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094] (2)

--- chapter_003/src/main/java/ru/job4j/collection/UsageArrayList.java
+++ chapter_003/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UsageArrayList {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         List<String> names = new ArrayList();
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
-        for (String name: names) {
+        for (String name : names) {
             System.out.println(name);
         }
     }
